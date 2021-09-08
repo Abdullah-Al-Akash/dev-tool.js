045cfe988417e48f4ab11f9b767b9bafa92660f5
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-08 23:12:31 +0600

    Added Requirements:

---
+++ dev-tool.js
@@ -0,0 +1,13 @@
+/*
+        1. Find the documentation of console tab Utilities API. And Read it carefully. Spend 30-45 Mins.
+        2. Easiest way to find out  the event listener attached to a DOM element.
+        3. How can you break the first line of a function without actually adding a breakpoint. Rater you will use a console API utilities.
+        4. Go to the source code and add a conditional break point.
+        5. What is accessibility?
+        6. What is a call stack?
+        7. What is the purposes of regular expressions?
+        8. What is the purposes of lighthouse tab of chrome devtool?
+        9. Simple website. You will have a input and button. Input field will take an email. Use regular expression to validate that user is inserting an email.(Find the RegEx in inline)
+        10. If email is valid display some green color text. If not email, show some error text.
+        11. Clone Issue Tracker Website from ProgrammingHero1's gitHub. And fix these Bug/Issue.
+*/
